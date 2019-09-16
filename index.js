1eb1842d6a1c22dc2627b00842bbc99a782ff2cd
manjioko <koandting@gmail.com>
2019-09-16 12:02:30 +0800

    聊天记录读写使用异步函数处理防止

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var fs = require("fs");
-var mysql  = require('mysql');  
+var mysql = require('mysql');
 var bodyParser = require('body-parser');//解析,用req.body获取post参数
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
@@ -11,21 +11,21 @@ var usernameToClient = [];
 // var userandpass = false;
 // var rs;
 //打开数据库
-var pool = mysql.createPool({     
-  host     : 'localhost',       
-  user     : 'root',              
-  password : 'wanzhongke',
-  port: '3306',                   
-  database: 'userlogin'
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'wanzhongke',
+  port: '3306',
+  database: 'login'
 });
-var query = function query(sql){
-  return new Promise(function(resolve,reject){
-    pool.getConnection(function(err,conn){
-      if(err) {
+var query = function query(sql) {
+  return new Promise(function (resolve, reject) {
+    pool.getConnection(function (err, conn) {
+      if (err) {
         reject(err);
-      }else {
-        conn.query(sql,function(err,rows){
-          if(err){
+      } else {
+        conn.query(sql, function (err, rows) {
+          if (err) {
             reject(err);
           } else {
             resolve(rows);
@@ -39,8 +39,10 @@ var query = function query(sql){
 }
 
 
+
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
@@ -54,16 +56,16 @@ app.all('*', function (req, res, next) {
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
 });
- 
-app.post('/Login',function(req,res){
-  let sql = 'select * from login where name='+'\"'+req.body.username+'\"';
-  query(sql).then(function(rows){
-    let username=req.body.username;
-    let password=req.body.password;
-    let isLogined=0;
-    if(rows.length) {
+
+app.post('/Login', function (req, res) {
+  let sql = 'select * from login_user_msg where username=' + '\"' + req.body.username + '\"';
+  query(sql).then(function (rows) {
+    let username = req.body.username;
+    let password = req.body.password;
+    let isLogined = 0;
+    if (rows.length) {
       console.log(rows)
-      if(rows[0].name===username&&rows[0].password===password&&rows[0].logined===isLogined){
+      if (rows[0].username === username && rows[0].password === password && rows[0].logined === isLogined) {
         res.send("OK");
       } else {
         res.send("fail");
@@ -72,120 +74,164 @@ app.post('/Login',function(req,res){
   });
 });
 
-app.post('/Register',function(req,res){
-  let sql = 'select * from login where name='+'\"'+req.body.username+'\"';
-  query(sql).then(function(rows){
-    let username=req.body.username;
-    let password=req.body.password;
+app.post('/Register', function (req, res) {
+  let sql = 'select * from login_user_msg where username=' + '\"' + req.body.username + '\"';
+  query(sql).then(function (rows) {
+    let username = req.body.username;
+    let password = req.body.password;
     // let isLogined=0;
     console.log(rows);
-    if(!rows.length) {
-      let reg = 'insert into login (name, logined, password) values(' + '\"' + username +'\",' + 0 + ',\"'+password+'\"'+')';
+    if (!rows.length) {
+      let reg = 'insert into login (username, logined, password) values(' + '\"' + username + '\",' + 0 + ',\"' + password + '\"' + ')';
       console.log(reg);
-      query(reg)
+      query(reg).catch(err => console.log("reg error: " + err));
       res.send("OK");
     } else {
       res.send("repeated");
     }
+  }).catch(err => console.log("regitser error: " + err));
+});
+
+//异步处理io
+
+//读出数据
+readFileFromSys = function(name,socketId) {
+  return new Promise(function(resolve,reject) {
+        fs.readFile(name, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+        fs.writeFile(name, "", function (err) {
+          if (err) {
+            res.status(500).send('Data writting is error...')
+            reject("data readding error");
+          }
+        });
+      } else {
+        if (data) {
+          let d = JSON.parse(data);
+          let sendData = JSON.stringify(d);
+          io.sockets.connected[socketId].emit('storeMessage', sendData);
+          resolve("data readding OK");
+          // console.log(sendData);
+        } else
+          console.log("json file is null.");
+          reject("json is null");
+      }
+    });
+  })
+}
+
+//写入数据
+writeFileToSys = function(path,str) {
+    return new Promise(function(resolve,reject){
+      fs.writeFile(path, str, function (err) {
+        if (err) {
+          res.status(500).send('Data writting is error...')
+          reject("data writing error")
+        }else{
+          resolve("data reading OK");
+        }
+      });
+    });
+
+}
+// 用户上线处理
+io.on("connection", function (socket) {
+  socket.on('logined message', function (msg) {
+    console.log(msg + " is connected.");
+    username[socket.id] = msg;
+    console.log(username);
+    if (usernameToClient.indexOf(msg) === -1) {
+      usernameToClient.push(msg);
+    }
+
+    let sql = 'update login_user_msg set logined=1 where username=' + '\"' + username[socket.id] + '\"';
+    console.log(sql);
+    query(sql).catch(err => console.log("connection sql error: " + err));
+
+    let name = './data/' + msg + '.json';
+    // fs.readFile(name, 'utf8', function (err, data) {
+    //   if (err) {
+    //     console.log(err);
+    //     fs.writeFile(name, "", function (err) {
+    //       if (err) { res.status(500).send('Data writting is error...') }
+    //     });
+    //   } else {
+    //     if (data) {
+    //       let d = JSON.parse(data);
+    //       let sendData = JSON.stringify(d);
+    //       io.sockets.connected[socket.id].emit('storeMessage', sendData);
+    //       // console.log(sendData);
+    //     } else
+    //       console.log("json file is null.");
+    //   }
+    // });
+    readFileFromSys(name,socket.id).catch(err => console.log(err));
+
+    console.log(usernameToClient);
+    // 将用户列表发送到登录的id上
+    io.sockets.connected[socket.id].emit('loginedUserList', usernameToClient);
+    // console.log("the data is emit" + storeData);
+    // io.sockets.connected[socket.id].emit('storeData',storeData);
+    // 将自己的登录情况广播到其他用户
+    socket.broadcast.emit('userManageAdd', username[socket.id]);
+    storeData = '';
+  });
+
+  // 掉线情况处理
+  socket.on('disconnect', function () {
+    if (username.hasOwnProperty(socket.id)) {
+      console.log(username[socket.id] + ' is disconnected');
+      // 如果在线人数不为0，继续删掉下线的用户
+      if (usernameToClient.length) {
+        usernameToClient = usernameToClient.filter(item => item !== username[socket.id]);
+        io.emit('userManageDel', username[socket.id])
+        // io.emit('userManageDel',usernameToClient);
+        let sql = 'update login_user_msg set logined=0 where username=' + '\"' + username[socket.id] + '\"';
+        console.log(sql);
+        query(sql).catch(err => console.log("disconnect error: " + err));
+        delete username[socket.id];
+        console.log(username);
+        return;
+      }
+      // 退出登录后，修改数据库Logined 值为0
+      // 这是为0 的情况，直接删掉对象值即可，这里需要注意。
+      delete username[socket.id];
+      console.log(username);
+
+    } else {
+      console.log('user disconnected');
+    }
+
+  });
+  // 会话处理
+  socket.on('chat message', function (msg) {
+    data = '<div>' + '<h3>' + username[socket.id] + '</h3>' + '<span id="otherMsg">' + msg + '</span>' + '</div>';
+    // 广播到其他用户
+    socket.broadcast.emit('bradcast', data);
+  });
+  //私聊实现
+  socket.on('privateChat', function (msg) {
+    // let socketId;
+    for (const key in username) {
+      data = '<div>' + '<h3>' + username[socket.id] + '</h3>' + '<span id="otherMsg">' + msg[1] + '</span>' + '</div>';
+      if (username[key] === msg[0]) {
+        io.sockets.connected[key].emit('privateChat', [username[socket.id], msg[1]]);
+      }
+    }
+  });
+  socket.on('storeMessage', function (msg) {
+    let str = JSON.stringify(msg[1], null, "\t");
+    let path = './data/' + msg[0] + '.json';
+    // fs.writeFile(path, str, function (err) {
+    //   if (err) { res.status(500).send('Data writting is error...') }
+    // });
+    writeFileToSys(path,str).catch(err => console.log(err));
+    // console.log(msg);
   });
 });
 
-// 用户上线处理
-io.on("connection", function(socket) {
-    socket.on('logined message',function(msg) {
-        console.log(msg + " is connected.");
-
-        username[socket.id] = msg;
-        console.log(username);
-        if(usernameToClient.indexOf(msg) === -1) {
-          usernameToClient.push(msg);
-        }
-
-        let sql = 'update login set logined=1 where name='+'\"'+username[socket.id]+'\"';
-        console.log(sql);
-        query(sql);
-
-        let name ='./data/'+ msg +'.json';
-        fs.readFile(name,'utf8',function (err, data) {
-          if(err){
-            console.log(err);
-            fs.writeFile(name,"",function(err){
-              if (err) {res.status(500).send('Data writting is error...')}
-            });
-          } else {
-            if(data) {
-              let d=JSON.parse(data);
-              let sendData = JSON.stringify(d);
-              io.sockets.connected[socket.id].emit('storeMessage',sendData);
-              // console.log(sendData);
-            }else
-              console.log("json file is null.");
-          }
-        });
-
-        console.log(usernameToClient);
-        // 将用户列表发送到登录的id上
-        io.sockets.connected[socket.id].emit('loginedUserList',usernameToClient);
-        // console.log("the data is emit" + storeData);
-        // io.sockets.connected[socket.id].emit('storeData',storeData);
-        // 将自己的登录情况广播到其他用户
-        socket.broadcast.emit('userManageAdd',username[socket.id]);
-        storeData = '';
-    });
-    
-    // 掉线情况处理
-    socket.on('disconnect', function(){
-        if(username.hasOwnProperty(socket.id)) {
-            console.log(username[socket.id] + ' is disconnected');
-            // 如果在线人数不为0，继续删掉下线的用户
-            if(usernameToClient.length) {
-                 usernameToClient = usernameToClient.filter( item => item !== username[socket.id]);
-                io.emit('userManageDel',username[socket.id])
-                // io.emit('userManageDel',usernameToClient);
-                let sql = 'update login set logined=0 where name='+'\"'+username[socket.id]+'\"';
-                console.log(sql);
-                query(sql);
-                delete username[socket.id];
-                console.log(username);
-                return;
-            }
-            // 退出登录后，修改数据库Logined 值为0
-            // 这是为0 的情况，直接删掉对象值即可，这里需要注意。
-            delete username[socket.id];
-            console.log(username);
-
-        } else {
-            console.log('user disconnected');
-        }
-        
-    });
-    // 会话处理
-    socket.on('chat message', function(msg){
-        data = '<div>' + '<h3>' + username[socket.id] + '</h3>' + '<span id="otherMsg">' + msg + '</span>' + '</div>';
-        // 广播到其他用户
-        socket.broadcast.emit('bradcast',data);
-    });
-    //私聊实现
-    socket.on('privateChat',function(msg){ 
-        // let socketId;
-        for (const key in username) {
-            data = '<div>' + '<h3>' + username[socket.id] + '</h3>' + '<span id="otherMsg">' + msg[1] + '</span>' + '</div>';
-            if(username[key] === msg[0]){
-                io.sockets.connected[key].emit('privateChat', [username[socket.id],msg[1]]);
-            }
-        }
-    });
-    socket.on('storeMessage',function(msg){
-      let str = JSON.stringify(msg[1],null,"\t");
-      let path = './data/'+msg[0]+'.json';
-      fs.writeFile(path,str,function(err){
-        if (err) {res.status(500).send('Data writting is error...')}
-      });
-      // console.log(msg);
-    });
-});
-
 // 监听3000端口
-http.listen(3000,function(){
-    console.log("listening on *:3000");
+http.listen(3000, function () {
+  console.log("listening on *:3000");
 });
